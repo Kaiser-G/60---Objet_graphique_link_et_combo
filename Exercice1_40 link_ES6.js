89e79bb4cd5155064a9669f649d30044ba01aa4e
Kaiser-G <kaiserg6969@gmail.com>
2024-02-26 17:18:26 +0100

    Mise en place exo 1

---
+++ Exercice1_40 link_ES6.js
@@ -0,0 +1,27 @@
+class Link {
+  Id_zone = "nc";
+  Id_link = "nc";
+  Class_link = "nc";
+  Url = "nc";
+  Texte_visible = "nc";
+  Target = "nc";
+  Append = "nc";
+
+  constructor(
+    id_zone = "nc",
+    id_link = "nc",
+    class_link = "nc",
+    url = "nc",
+    texte_visible = "nc",
+    target = "nc",
+    append = "nc"
+  ) {
+    this.Id_zone = id_zone;
+    this.Id_link = id_link;
+    this.Class_link = class_link;
+    this.Url = url;
+    this.Texte_visible = texte_visible;
+    this.Target = target;
+    this.Append = append;
+  }
+}
