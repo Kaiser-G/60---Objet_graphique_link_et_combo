9122ced08bd9e4203ba51315aaa7eebec1e8f737
Kaiser-G <kaiserg6969@gmail.com>
2024-02-27 15:03:29 +0100

    Exo 2 bien avancé

--- Exercice2_50 Combo_ES6.js
+++ Exercice2_50 Combo_ES6.js
@@ -8,20 +8,23 @@ class Combo {
   generer() {
     if (this.id_zone != "") {
       let code_generer = document.createElement("select");
-      document.createElement("option").appendChild(code_generer);
 
       if (this.id_select != "") {
         code_generer.id = this.id_select;
+        // document.createElement("option").appendChild(code_generer.id_select);
       }
       if (this.class_select != "") {
         code_generer.className = this.class_select;
       }
-      code_generer.href = this.url;
-      code_generer.target = this.target;
-      code_generer.innerText = this.texte_visible;
-      if (!this.append) {
-        document.getElementById(this.id_zone).innerHTML = "";
+      if (this.data != "") {
+        this.data.forEach((value) => {
+          let option = document.createElement("option");
+          option.innerHTML = value[1];
+          option.value = value[0];
+          code_generer.appendChild(option);
+        });
       }
+
       document.getElementById(this.id_zone).appendChild(code_generer);
     } else {
       throw new Error(
